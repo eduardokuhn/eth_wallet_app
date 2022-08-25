6d3c1e4c639e91a2c4edd9af10541bbda74b2c53
Eduardo de Oliveira Kuhn <edukuhn@publicisgroupe.net>
2022-08-25 14:48:49 +0200

    First commit2

---
+++ frontend/app/src/main/java/com/example/ethwalletapp/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.example.ethwalletapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val Gray21 = Color(0x202832)
+val Gray24 = Color(0x080A0C)
+val Gradient06 = listOf(
+  Color(0x8AD4EC),
+  Color(0xEF96FF),
+  Color(0xFF56A9),
+  Color(0xFFAA6C),
+)
